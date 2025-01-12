7eb447f119263ccb52697dc5b2430a173b6f16cb
David <toughdave@gmail.com>
2025-01-11 21:14:35 -0500

    Added more code and other edits

--- clothing.php
+++ clothing.php
@@ -1,6 +1,66 @@
-<?php 
-echo "This is the clothing section of the website."; 
-echo "We have a wide variety of clothing items such as:";
-echo "T-shirts, jeans, jackets, and more!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="main1">
+        <section class="container"> 
+            <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+                <div class="container-fluid">
+                <a class="navbar-brand" href="#">Store Front</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="clothing.php">Clothing<span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact-us.html">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cart.html">My Cart</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Options</a>
+                        <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </li>
+                    </ul>
+                    <form class="d-flex">
+                    <input class="form-control me-sm-2" type="search" placeholder="Search for products">
+                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+                </div>
+            </nav>
+        </section>
+    
 
-?>
+        <?php 
+        echo "This is the clothing section of the website."; 
+        echo "We have a wide variety of clothing items such as:";
+        echo "T-shirts, jeans, jackets, and more!";
+
+        ?>
+
+        
+        <footer>
+            <p>&copy; 2025 - All rights reserved</p>
+        </footer>
+    </div>
+</body>
+</html>
+
